f0d2abd85264a235965f4a32766f5035a09675f4
Renan Pereira <renanop@poli.ufrj.br>
2020-07-30 15:32:49 -0300

    adding pagination system and improving admin interfaces

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -7,21 +7,16 @@ const slugify = require('slugify');
 
 router.get('/admin/articles', (req, res) => {
     Article.findAll({
-        include: [{model: Category}]
+        include: [{ model: Category }]
     }).then(articles => {
-        res.render('admin/articles/index', {articles:articles});
+        res.render('admin/articles/index', { articles: articles });
     });
 });
 
-Article.findAll({
-    include: [{model: Category}]
-}).then(articles => {
-    console.log(articles);
-});
 
-router.get('/admin/articles/new', (req,res) => {
+router.get('/admin/articles/new', (req, res) => {
     Category.findAll().then(categories => {
-        res.render("admin/articles/new",{categories:categories});
+        res.render("admin/articles/new", { categories: categories });
     });
 });
 
@@ -49,16 +44,16 @@ router.post('/articles/save', (req, res) => {
 // Deleting categories
 router.post('/articles/delete', (req, res) => {
     var id = req.body.id;
-    if(id != undefined) {
-        if(!isNaN(id)) {
+    if (id != undefined) {
+        if (!isNaN(id)) {
             Article.destroy({
                 where: {
-                    id:id
+                    id: id
                 }
             }).then(() => {
                 res.redirect('/admin/articles');
             });
-            
+
         } else {
             res.redirect('/admin/articles');
         }
@@ -67,10 +62,88 @@ router.post('/articles/delete', (req, res) => {
     }
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if (isNaN(id)) {
+            res.redirect("/admin/articles");
+        }
+
+        if (article != undefined) {
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", { article: article, categories: categories });
+            });
+        } else {
+            res.redirect("/admin/articles");
+        }
+    }).catch(err => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/update", (req,res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    console.log(category);
+    
+    Article.update({title: title, slug:slugify(title), body: body, categoryId: category}, {
+        where: {
+            id:id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num;
+    var offset = 0;
+    var articlesPerPage = 4;
+
+    if(isNaN(page) || page == 1) {
+        offset = 0;
+    } else {
+        offset = (parseInt(page)-1) * articlesPerPage;
+    }
 
 
 
+    Article.findAndCountAll({
+        limit: articlesPerPage,
+        offset: offset,
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
 
 
+        var next;
+
+        if(offset + articlesPerPage >= articles.count) {
+            next = false;
+        } else {
+            next = true;
+        }
+
+        console.log(offset);
+        console.log(articlesPerPage);
+        console.log(Article.count());
+        console.log(next);
+    
+        var result = {
+            next: next,
+            articles: articles,
+            page: parseInt(page)
+        }
+
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page",{result:result, categories:categories});
+        })
+        
+    })
+})
 
 module.exports = router;
